fd2aeb72579a7c84d333c3eeccc9d745dfe1377c
Ertinox-08 <aymericdelange80@gmail.com>
2023-08-31 21:08:38 +0200

    Auto sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,34 @@
+import {PROJECTS} from "@/data/data";
+
+export default function sitemap() {
+    return [
+        {
+            url: 'https://aymericdelange.fr/',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://aymericdelange.fr/about',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://aymericdelange.fr/project',
+            lastModified: new Date(),
+        },
+        PROJECTS.map(project => ({
+            url: `https://aymericdelange.fr/project/${project.slug}`,
+            lastModified: new Date(),
+        })),
+        {
+            url: 'https://aymericdelange.fr/skills',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://aymericdelange.fr/stats',
+            lastModified: new Date(),
+        },
+        {
+            url: 'https://aymericdelange.fr/experience',
+            lastModified: new Date(),
+        },
+    ]
+}
