3050d8e82be2649f13e59e47cf1eb164c4299f0b
Ertinox-08 <aymericdelange80@gmail.com>
2023-08-31 21:08:47 +0200

    Update

--- src/components/ButtonLink.jsx
+++ src/components/ButtonLink.jsx
@@ -13,14 +13,14 @@ export default function ButtonLink({name, slug}) {
     const isActive = false
     if (isActive) {
         return (
-            <Link href={`/${slug}`} className="flex flex-col items-center transition-all py-[0.5rem] px-[1rem] uppercase font-black bg-white text-stone-800 rounded-full text-xs">
+            <Link href={`/${slug}`} className="flex flex-col items-center transition-all py-[0.5rem] px-[1rem] uppercase font-black bg-white text-stone-800 rounded-xl text-xs">
                 {/*<Home className="w-[40px] h-[40px]"/>*/}
                 <span className="text-center">{name}</span>
             </Link>
         )
     } else {
         return (
-            <Link href={`/${slug}`} className="flex flex-col items-center transition-all py-[0.5rem] px-[1rem] uppercase font-black bg-stone-800 text-white hover:bg-white hover:text-stone-800 rounded-full text-xs">
+            <Link href={`/${slug}`} className="flex flex-col items-center transition-all py-[0.5rem] px-[1rem] uppercase font-black bg-stone-800 text-white hover:bg-white hover:text-stone-800 rounded-xl text-xs">
                 <span className="text-center">{name}</span>
             </Link>
         )
