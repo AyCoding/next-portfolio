e98715c8de81dcaed0783bbde41ed8ef2afb6147
Ertinox-08 <aymericdelange80@gmail.com>
2023-09-01 15:45:25 +0200

    Add Vercel Web analysis

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,13 @@
 import './globals.css'
 import Aside from "@/components/Aside";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* className={inter.className}*/}
       <body>{children}
       <Aside/>
+      <Analytics/>
       </body>
     </html>
   )
