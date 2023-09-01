54f8a2c52b0860294974526b6821cd066cff7dda
Ertinox-08 <aymericdelange80@gmail.com>
2023-09-01 16:05:31 +0200

    Remove analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,11 @@
 import './globals.css'
 import Aside from "@/components/Aside";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}
       <Aside/>
-      <Analytics/>
       </body>
     </html>
   )
