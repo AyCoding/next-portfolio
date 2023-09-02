dd1b595e37b590af3fa56e3a29c3504862fbf1ee
Ertinox-08 <aymericdelange80@gmail.com>
2023-09-02 19:24:03 +0200

    Web analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,14 @@
 import './globals.css'
 import Aside from "@/components/Aside";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}
+      <body>
+      {children}
       <Aside/>
+      <Analytics/>
       </body>
     </html>
   )
