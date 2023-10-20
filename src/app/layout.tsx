6ffa18c7ec1ef89b5b06c1d58115b2d439d01a47
Ertinox-08 <aymericdelange80@gmail.com>
2023-10-20 16:04:07 +0200

    Remove Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import './globals.css'
 import Aside from "@/components/Aside";
 import { Analytics } from '@vercel/analytics/react';
 import React, {ReactNode} from "react";
-import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 interface RootLayoutProps {
     children: ReactNode;
@@ -11,9 +10,6 @@ interface RootLayoutProps {
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
     return (
         <html lang="fr">
-        <head>
-            <GoogleAnalytics GA_MEASUREMENT_ID='G-HB9TKGVWWK'/>
-        </head>
         <body>
         {children}
         <Aside />
