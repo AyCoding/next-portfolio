eaa0e3bfef84db8c16ed3361d6b693963e533d7c
Ertinox-08 <aymericdelange80@gmail.com>
2023-10-20 15:25:08 +0200

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import Aside from "@/components/Aside";
 import { Analytics } from '@vercel/analytics/react';
 import React, {ReactNode} from "react";
+import GoogleAnalytics from '@/components/GoogleAnalytics';
 
 interface RootLayoutProps {
     children: ReactNode;
@@ -10,6 +11,7 @@ interface RootLayoutProps {
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
     return (
         <html lang="fr">
+        <GoogleAnalytics GA_MEASUREMENT_ID='G-HB9TKGVWWK'/>
         <body>
         {children}
         <Aside />
