8490c11b19c776878f1b632b45067b2be46e7bbd
Aymeric DE LANGE <aymericdelange80@gmail.com>
2024-01-12 14:16:08 +0100

    Update experiences

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -9,13 +9,25 @@ export const metadata = {
 export default function Experience() {
     const EXPERIENCES = [
         {
+            id: 3,
+            name: "Institut de Médecine du Sport de Troyes",
+            type: "Stage",
+            date: "08/01/2024 - 09/02/2024",
+            location: "A distance",
+            desc: "",
+            url: "https://imstroyes.fr/",
+            languages: [
+                {id: 1, lang: "NuxtJS"}
+            ]
+        },
+        {
             id: 2,
             name: "Institut de Médecine du Sport de Troyes",
             type: "Stage",
             date: "05/06/2023 - 06/07/2023",
             location: "A distance",
             desc: "Pendant mon stage d'un mois lors de ma première année en BTS SIO SLAM, j'ai eu l'opportunité de contribuer à la conception et à la refonte du site web de l'Institut de Médecine du Sport de Troyes.",
-            url: "https://imstroyes.aymericdelange.fr/",
+            url: "https://imstroyes.fr/",
             languages: [
                 {id: 1, lang: "VueJS"}
             ]
@@ -24,7 +36,7 @@ export default function Experience() {
             id: 1,
             name: "Webmaster Sage-femme",
             type: "Maison",
-            date: "07/2022 - 09/2023",
+            date: "07/2022 - 09/2022",
             location: "A distance",
             desc: "Création du site/design pour une sage-femme basé sur Troyes.",
             url: "https://canaud-emilie-sage-femme.fr/",
@@ -57,24 +69,23 @@ export default function Experience() {
                         <div className="font-light flex-1 md:mr-5">
                             <p className="font-normal">{experience.desc}</p>
 
-                            <div className="flex gap-2 py-5 md:py-2 flex-wrap text-sm font-normal ">
+                            <div className="flex justify-between gap-2 py-5 md:py-2 flex-wrap text-sm font-normal ">
                                 {experience.languages && experience.languages.map(language => (
                                     <div key={language.id}
                                          className="border border-stone-700 gap-2 rounded-full px-4 py-0.5">
                                         {language.lang}
                                     </div>
                                 ))}
+                                {experience.url && (
+                                    <Link href={experience.url}
+                                          target="_blank" rel="noreferrer"
+                                          className="flex gap-1 items-center font-medium hover:text-gray-700 transition-all">
+                                        <ExternalLink/>
+                                        Site web
+                                    </Link>
+                                )}
                             </div>
                         </div>
-
-                        {experience.url && (
-                            <Link href={experience.url}
-                                  target="_blank" rel="noreferrer"
-                                  className="flex gap-1 items-center font-medium hover:text-gray-700 transition-all">
-                                <ExternalLink/>
-                                Site web
-                            </Link>
-                        )}
                     </div>
                 </div>
             ))}
