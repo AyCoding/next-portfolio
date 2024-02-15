7b999193cf67015c21f760b7617fbbe3f70561a3
Aymeric DE LANGE <aymericdelange80@gmail.com>
2024-02-15 19:47:13 +0100

    change about text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+
 
 export const metadata = {
     title: 'Aymeric DE LANGE - A propos de moi',
@@ -7,16 +7,19 @@ export const metadata = {
 
 export default function About() {
     return (
-        <div className="container max-w-[1200px] mx-auto pb-[40px] px-5 md:px-2 sm:flex sm:flex-col sm:justify-center sm:h-screen">
+        <div
+            className="container max-w-[1200px] mx-auto pb-[40px] px-5 md:px-2 sm:flex sm:flex-col sm:justify-center sm:h-screen">
             <p className="text-5xl font-black py-6">Découvrez-moi</p>
 
             <section className="w-full h-52 py-5 md:rounded-3xl">
                 <p className="text-xl text-gray-700">
-                    Bonjour, je suis Aymeric DE LANGE, développeur front-end passionné par la programmation web depuis 2018. Au
-                    fil des années, j&apos;ai acquis une solide expérience en développement web front-end, ce qui m&apos;a permis
-                    de créer divers projets intéressants. Je vous invite à découvrir mes projets. N&apos;hésitez
-                    pas à me contacter si vous avez des questions ou si vous êtes intéressé par une collaboration.
+                    Bonjour, je suis Aymeric DE LANGE, 20 ans, développeur front-end passionné par la programmation web
+                    depuis 2018. Actuellement en <span className="font-medium">BTS Services informatiques aux organisations option B solutions
+                    logicielles et applications métiers (SIO SLAM)</span>. Je vous invite à découvrir mes projets.
+                    N&apos;hésitez pas à me contacter si vous avez des questions ou si vous êtes intéressé par une
+                    collaboration.
                 </p>
+                <p></p>
             </section>
         </div>
     )
