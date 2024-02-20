a8a5a2756e205dac4f22a3409ac3a602989d6a1a
Aymeric DE LANGE <aymericdelange80@gmail.com>
2024-02-20 20:16:26 +0100

    remove `lastModified: new Date()`

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -4,35 +4,27 @@ export default function sitemap() {
     return [
         {
             url: 'https://aymericdelange.fr/',
-            lastModified: new Date(),
         },
         {
             url: 'https://aymericdelange.fr/about',
-            lastModified: new Date(),
         },
         {
             url: 'https://aymericdelange.fr/project',
-            lastModified: new Date(),
         },
         ...PROJECTS.map(project => ({
             url: `https://aymericdelange.fr/project/${project.slug}`,
-            lastModified: new Date(),
         })),
         {
             url: 'https://aymericdelange.fr/skills',
-            lastModified: new Date(),
         },
         {
             url: 'https://aymericdelange.fr/stats',
-            lastModified: new Date(),
         },
         {
             url: 'https://aymericdelange.fr/experience',
-            lastModified: new Date(),
         },
         {
             url: 'https://aymericdelange.fr/contact',
-            lastModified: new Date(),
         },
     ]
 }
